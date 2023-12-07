c19dd0630a42e990c07bb267e5aff518d49a8462
taylerderden <danizlobin_2004@mail.ru>
2023-12-07 14:46:12 +0400

    with filtr sort search

--- WpfAppLVGV/Windows/lViewWindow.xaml.cs
+++ WpfAppLVGV/Windows/lViewWindow.xaml.cs
@@ -22,7 +22,65 @@ namespace WpfAppLVGV.Windows
         public lViewWindow()
         {
             InitializeComponent();
+            Update();
+
+            cbSort.Items.Add("По возрастанию");
+            cbSort.Items.Add("По убыванию");
+
+            //List<Task> tasksCompletes = CoreModel.init().Tasks.ToList();
+            //tasksCompletes.Insert(0, new Task { TasksComplete = "+" });
+
+            cbFiltr.Items.Add("мужчины");
+            cbFiltr.Items.Add("женщины");
+            cbFiltr.Items.Add("все");
+        }
+
+        private void Update()
+        {
             ListViewClient.ItemsSource = CoreModel.init().Clients.ToList();
+        }
+        private void UpdateSearch()
+        {
+            IEnumerable<Client> clients = CoreModel.init().Clients.Where(p => p.FirstName.Contains(tbSearch.Text) || p.LastName.Contains(tbSearch.Text));
+
+            switch (cbSort.SelectedIndex)
+            {
+                case 0:
+                    clients = clients.OrderBy(p => p.FirstName);
+                    break;
+                case 1:
+                    clients = clients.OrderByDescending(p => p.FirstName);
+                    break;
+            }
+
+            if (cbFiltr.SelectedItem == "мужчины")
+            {
+                clients = CoreModel.init().Clients.Where(p => p.GenderCode.Contains("м"));
+            }
+            else if (cbFiltr.SelectedItem == "женщины")
+            {
+                clients = CoreModel.init().Clients.Where(p => p.GenderCode.Contains("ж"));
+            }
+            else
+            {
+                Update();
+            }
+
+            ListViewClient.ItemsSource = clients.ToList();
+        }
+        private void tbSearchChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateSearch();
+        }
+
+        private void cbSortChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateSearch();
+        }
+
+        private void cbFiltrChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateSearch();
         }
     }
 }
